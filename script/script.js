9222e3d3d46096b9e609e5faa66ced1bfd0c40f1
Usagino <yuki.itoh@front-artgraph.com>
2018-07-24 13:30:03 +0900

    update

--- script/script.js
+++ script/script.js
@@ -29,11 +29,53 @@ let down = "down";
 let up = "up";
 let animationEnd = 'webkitAnimationEnd oanimationend msAnimationEnd animationend';
 
-let text = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+let titlelist = [
+  "阿蘇外輪山",
+  "雲海テラス",
+  "東京スカイツリー",
+  "高ボッチ高原",
+  "山形県川尻",
+  "鳥海山",
+  "富士山",
+  "津別峠"
+]
+let textlist = [
+  "阿蘇山は日本の九州の中央部、熊本県阿蘇地方に位置する活火山です。外輪山と数個の中央火口丘から成り、外輪山は南北25km、東西18kmに及び世界最大級の面積380km2の広大なカルデラ地形（鍋型）を形成されています。",
+  "2",
+  "3",
+
+];
+let maplist = [
+  "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13401.701643407687!2d131.07535203533118!3d32.88691894502153!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3540d87b844258f9%3A0x374a44e66d271103!2z6Zi_6JiH5bGx!5e0!3m2!1sja!2sjp!4v1532398118818",
+
+  "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d306814.7707016628!2d138.13804251735667!3d35.89175235814968!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x601c5c75ffa3faf9%3A0x65163ad3ca5a284d!2z6Zuy5rW344OG44Op44K5!5e0!3m2!1sja!2sjp!4v1532399083475",
+
+  "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3239.6567846113153!2d139.80851171536256!3d35.71006268018759!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188ed0d12f9adf%3A0x7d1d4fb31f43f72a!2z5p2x5Lqs44K544Kr44Kk44OE44Oq44O8!5e0!3m2!1sja!2sjp!4v1532399589442",
+
+  "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3222.4155366052782!2d138.03257031537373!3d36.13209438009164!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x601d05b30ebd78cf%3A0xd579babbb8e8faa1!2z6auY44Oc44OD44OB6auY5Y6f!5e0!3m2!1sja!2sjp!4v1532400355954",
+
+  "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12446.151708422192!2d139.92559843779674!3d38.75136442349326!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x5f8c1cad3ecad92b%3A0xd25ffef7d99f57dc!2z44CSOTk5LTc2NDQg5bGx5b2i55yM6ba05bKh5biC5bed5bC7!5e0!3m2!1sja!2sjp!4v1532401206135",
+
+  "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12385.283245272738!2d140.04207853795373!3d39.09916572263874!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x5f8eed559b6f460d%3A0x8e67ba4656d9ec4d!2z6bOl5rW35bGx!5e0!3m2!1sja!2sjp!4v1532401490050",
+
+  "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d16704.47314230657!2d138.87622737514232!3d35.417619435326884!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60196f92596a144d%3A0x2f4af010626a45dd!2z5a-M5aOr5bGx44Ks44O844OH44Oz44Ob44OG44Or!5e0!3m2!1sja!2sjp!4v1532401680472",
+
+  "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d46227.529777570984!2d144.18509935483326!3d43.60194800537384!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x5f6d633df1d25c13%3A0xd12647de4103572f!2z5rSl5Yil5bOg5bGV5pyb5pa96Kit!5e0!3m2!1sja!2sjp!4v1532405536472",
+
+];
+let imagelist =[
+  "material/cloud_1.jpg",
+  "material/cloud_2.jpg",
+  "material/cloud_3.jpg",
+  "material/cloud_4.jpg",
+  "material/cloud_5.jpg",
+  "material/cloud_6.jpg",
+  "material/cloud_7.jpg",
+  "material/cloud_8.jpg",
+]
 
 $(window).on('load', function() {
   $(".bucket .screen").on(animationEnd, function() {
-    console.log("HelloWorld");
     $(".loading").addClass('trash')
   });
 });
@@ -42,7 +84,6 @@ $(function() {
   $(window).scroll(function() {
     let pos = $(this).scrollTop();
     h_boundary = start - pos;
-    console.log(h_boundary);
     if (1 < h_boundary) {
 
       arrow_remove("down");
@@ -80,8 +121,19 @@ $(function() {
 
   $(".clouds_cloud").click(function() {
     $(".clouds_wrap").addClass('clouds_anime');
-    $(".clouds_contentwrap .text p").text(text);
+
   });
+
+
+  for( let i=0; i<=7; i++) {
+    $(".clouds_c"+ i).click(function() {
+      $(".clouds_contentwrap .text h1").text(titlelist[i]);
+      $(".clouds_contentwrap .text p").text(textlist[i]);
+      $(".clouds_contentwrap .map iframe").attr('src',maplist[i]);
+      $(".clouds_contentwrap .image img").attr('src',imagelist[i]);
+    });
+  }
+
   $(".clouds_wrap_close").click(function() {
     $(".clouds_wrap").removeClass('clouds_anime');
   });
